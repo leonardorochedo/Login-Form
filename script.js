4b6630841647870a94144776405b9a45dd918213
leonardorochedo <learnir@outlook.com.br>
2022-02-15 12:33:25 -0300

    ultimos detalhes para mobile

---
+++ script.js
@@ -0,0 +1,8 @@
+function showPassword() {
+    let tipo = document.getElementById("password")
+    if (tipo.type == "password") {
+        tipo.type = "text"
+    } else {
+        tipo.type = "password"
+    }
+}
